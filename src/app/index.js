daec1333624dee00b1d2d05ad7c5f918c16653af
chjeong231 <chjeong@ccfocusone.com>
2020-05-17 05:04:18 +0900

    구조 변경 및 로그인보드 삭제

--- src/app/index.js
+++ src/app/index.js
@@ -4,18 +4,15 @@ import uiRoute from "@uirouter/angularjs";
 
 import navBar from "./component/navBar/navBar";
 
-import loginBoard from "./component/board/loginBoard/loginBoard";
-
-import mainBoard from "./component/board/mainBoard/mainBoard";
-import podList from "./component/board/mainBoard/podList/podList";
-import topicList from "./component/board/mainBoard/topicList/topicList";
-import topicDetail from "./component/board/mainBoard/topicDetail/topicDetail";
+import mainBoard from "./component/board/mainBoard";
+import podList from "./component/board/podList/podList";
+import topicList from "./component/board/topicList/topicList";
+import topicDetail from "./component/board/topicDetail/topicDetail";
 
 angular.module("ngApp",[uiRoute])
     .config(($stateProvider, $urlRouterProvider) => {
         $urlRouterProvider.otherwise('/');
 
-        $stateProvider.state('login', loginBoard);
         $stateProvider.state('main', mainBoard);
 
         $stateProvider.state('list', topicList);
